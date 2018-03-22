8190cccc286892d5040025d496582ce88663bc8d
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-03-22 17:13:53 -0600

    polaroid finished

--- src/containers/Projects/Projects.style.js
+++ src/containers/Projects/Projects.style.js
@@ -2,15 +2,22 @@ import styled from 'styled-components';
 
 export const Polaroid = styled.div`
     display: inline-block;
+    overflow: hidden;
     > img {
         height: 40vh;
         width: 40vh;
         padding: 15px 10px 10px 10px;
     }
-    &:hover {
+    :hover {
         & > img {
             transform: scale(1.2);   
         }
+    }
+    
+    > h2 {
+        padding: 20px 0 20px 0;
+        border: 1px dotted black;
+        border-radius: 10px;
     }
     background-color: tan;
     text-align: center;
