6f9f9fd89b945faa2e7860932f22039f1ade472e
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-03-22 00:33:43 -0600

    Revealing text component, not changing state onEnter

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,14 @@
 import React, {Component, PropTypes} from 'react';
-import styled from 'styled-components';
-import { Container } from '../../theme/grid';
+import {Container} from '../../theme/grid';
 
-const Image = styled.img`
-    width: 100%;
-    height: 20%;
-    `;
+import {
+    Image,
+    HeroImage,
+    RevealP
+} from './Home.style';
+
+import WhenInView from '../../components/WhenInView/WhenInView';
+
 
 export default class Home extends Component {
     static propTypes = {};
@@ -13,17 +16,60 @@ export default class Home extends Component {
     render() {
         return (
             <Container>
-                <div>
-                    <Image src={require('../../assets/Me.jpg')} alt="Me"/>
-                    <p>
+                {/*<Image src={require('../../assets/Me.jpg')} alt="Me"/>*/}
+                <HeroImage>
+                    <h1>LeChristopher Blackwell</h1>
+                    <h2>Fullstack Software Engineer</h2>
+                </HeroImage>
+                <WhenInView>
+                    { ({ isInView })  =>
+                        <RevealP hide={isInView}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum nulla est,
                         et mattis leo pellentesque malesuada. Vivamus sodales accumsan orci, nec malesuada nisi
                         molestie quis. Donec turpis lectus, congue sit amet sem tempus, varius auctor ex. Sed est
                         nulla, consectetur sit amet dignissim ac, vulputate at leo. Sed dignissim eleifend risus a
                         pellentesque. Cras imperdiet tortor non nisi placerat lobortis. Sed non orci tincidunt
                         ante mollis ultricies sit amet sed tortor. Vivamus at enim tellus. Aliquam erat volutpat.
-                    </p>
-                </div>
+                        </RevealP>
+                    }
+                </WhenInView>
+                <WhenInView>
+                    { ({ isInView })  =>
+                        <RevealP hide={!isInView}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum nulla est,
+                            et mattis leo pellentesque malesuada. Vivamus sodales accumsan orci, nec malesuada nisi
+                            molestie quis. Donec turpis lectus, congue sit amet sem tempus, varius auctor ex. Sed est
+                            nulla, consectetur sit amet dignissim ac, vulputate at leo. Sed dignissim eleifend risus a
+                            pellentesque. Cras imperdiet tortor non nisi placerat lobortis. Sed non orci tincidunt
+                            ante mollis ultricies sit amet sed tortor. Vivamus at enim tellus. Aliquam erat volutpat.
+                        </RevealP>
+                    }
+                </WhenInView>
+                <WhenInView>
+                    { ({ isInView })  =>
+                        <RevealP hide={!isInView}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum nulla est,
+                            et mattis leo pellentesque malesuada. Vivamus sodales accumsan orci, nec malesuada nisi
+                            molestie quis. Donec turpis lectus, congue sit amet sem tempus, varius auctor ex. Sed est
+                            nulla, consectetur sit amet dignissim ac, vulputate at leo. Sed dignissim eleifend risus a
+                            pellentesque. Cras imperdiet tortor non nisi placerat lobortis. Sed non orci tincidunt
+                            ante mollis ultricies sit amet sed tortor. Vivamus at enim tellus. Aliquam erat volutpat.
+                        </RevealP>
+                    }
+                </WhenInView>
+                <WhenInView>
+                    { ({ isInView })  =>
+                        <RevealP hide={!isInView}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus interdum nulla est,
+                            et mattis leo pellentesque malesuada. Vivamus sodales accumsan orci, nec malesuada nisi
+                            molestie quis. Donec turpis lectus, congue sit amet sem tempus, varius auctor ex. Sed est
+                            nulla, consectetur sit amet dignissim ac, vulputate at leo. Sed dignissim eleifend risus a
+                            pellentesque. Cras imperdiet tortor non nisi placerat lobortis. Sed non orci tincidunt
+                            ante mollis ultricies sit amet sed tortor. Vivamus at enim tellus. Aliquam erat volutpat.
+                        </RevealP>
+                    }
+                </WhenInView>
+
             </Container>
         )
     }
