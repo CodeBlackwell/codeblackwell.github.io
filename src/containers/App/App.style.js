6c2e7d02fa2f36dc184b081956393b89c17b18f7
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-03-23 08:37:11 -0600

    wip

---
+++ src/containers/App/App.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+//@TODO: background-image: url({'my.picture.com'})
+export const Background = styled.div`
+    position: fixed;
+    left: 10vw;
+    width: 80vw;
+    top: 10vh;
+    height: 80vh;
+    background-color: #610078;
+    background-size: 80%;
+    background-repeat: no-repeat;
+    background-position: center;
+    z-index: -99;
+`;
