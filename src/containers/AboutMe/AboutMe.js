60510912c67d56dbdc6286cfdc51d0d202dad110
LeChristopher Blackwell <codeblackwell@gmail.com>
2018-03-23 14:06:16 -0600

    About me scaffolded

--- src/containers/AboutMe/AboutMe.js
+++ src/containers/AboutMe/AboutMe.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Container } from '../../theme/grid';
-import { Title } from './AboutMe.style'
+import { H2, H1 } from '../../theme/types';
+import { Title } from './AboutMe.style';
 
 class AboutMe extends Component {
     render() {
         return (
             <Container>
                 <Title>About Me</Title>
+                <H1 align="right">Do you like to stay active? Haha I can't help it!!</H1>
+                <H2 align="right">Check out </H2>
             </Container>
         );
     }
--- src/containers/AboutMe/AboutMe.style.js
+++ src/containers/AboutMe/AboutMe.style.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 import { tomato } from '../../theme/variables';
+import { Relative } from '../../theme/grid';
 
 export const Title = styled.h1`
     margin-top: 0;
     color: ${tomato}
+`;
+
+export const VideoContainer = styled(Relative)`
+    width: 70%;
+    flex-shrink: 0;
+    flex-grow: 0;
 `;
